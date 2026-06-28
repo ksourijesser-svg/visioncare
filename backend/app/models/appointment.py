bde568d52c861d460ada61448f10c47adffefd81
Jesser Ksouri <ksouri.jesser@gmail.com>
2026-06-28 16:22:40 +0100

    auto: save

--- backend/app/models/appointment.py
+++ backend/app/models/appointment.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Enum, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 import enum
